6160e0ba8ef8faf7326e30887e756332ea1a4eb1
Arnaud SJongers <asjongers@gmail.com>
2018-03-12 10:33:27 +0100

    removed x buttons on pikaday

--- app/components/range-filters.js
+++ app/components/range-filters.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    resetStartDate: function() {
-      this.set('startDate', null);
-    },
-    resetEndDate: function() {
-      this.set('endDate', null);
-    }
-  }
+
 });
