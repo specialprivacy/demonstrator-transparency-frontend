fb99bf51d7f920896209d4379000b0e8b1b921d6
Nóra Szepes <nora.szepes@tenforce.com>
2018-05-11 09:08:23 +0200

    checkbox-filters are now collapsable

--- app/components/range-filters.js
+++ app/components/range-filters.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-
+  showCheckboxes: false,
+  actions: {
+    showCheckboxesAction() {
+      this.toggleProperty('showCheckboxes');
+    }
+  }
 });
